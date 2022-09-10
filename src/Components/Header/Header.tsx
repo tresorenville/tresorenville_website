d2ae2a0a4537e77015de0ab394d45e952f34e7b7
git.youn <git.youn@gmail.com>
2022-09-10 15:11:52 +0200

    nav menu position

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -8,15 +8,15 @@ function Header() {
   return (
     <header className="p-2 border-b border-gray-200 shadow-sm">
       <div className="flex flex-col items-center">
-        <div className="flex flex-row items-center mb-4">
+        <div className="flex flex-row items-center mb-6">
           <img className="inline-block h-20 md:h-28 ml-2" src={logo_day_lantern} alt="Tresor en ville Lantern" />
           <div className="flex flex-col sm:flex-row items-start">
             <img className="inline-block h-9 md:h-14 ml-4 md:ml-8 sm:mt-6" src={logo_day_title_first_part} alt="Tresor en ville Title part 1" />
             <img className="inline-block h-9 md:h-14 ml-6 sm:ml-3 sm:mt-6" src={logo_day_title_second_part} alt="Tresor en ville Title part 2" />
           </div>
         </div>
-        <nav className="md:ml-auto">
-          <ul className="flex justify-around mt-2 md:mt-0 md:space-x-6">
+        <nav className="">
+          <ul className="flex justify-around space-x-6">
             <li>
               <NavLink to="/"
                 className={({ isActive }) => {
@@ -25,19 +25,23 @@ function Header() {
               }}>Accueil</NavLink>
             </li>
             <li>
-              <NavLink to="/about"
-                className={({ isActive }) => {
-                  if (isActive) return "p-2 rounded-md text-white bg-gray-500"
-                  else return "p-2 rounded-md hover:text-white hover:bg-gray-500"
-              }}>À propos</NavLink>
+              <div className="border-l border-gray-200 block pl-6">
+                <NavLink to="/about"
+                  className={({ isActive }) => {
+                    if (isActive) return "p-2 rounded-md text-white bg-gray-500"
+                    else return "p-2 rounded-md hover:text-white hover:bg-gray-500"
+                }}>À propos</NavLink>
+              </div>
+            </li>
+            <li>
+              <div className="border-l border-gray-200 block pl-6">
+                <a href="https://github.com/tresorenville/tresorenville_website" rel="external nofollow" title="Code source sur GitHub">
+                  <img src={githubMark} alt="Github Mark" className="h-6 w-6" />
+                </a>
+              </div>
             </li>
           </ul>
         </nav>
-        <div className="border-l border-gray-200 hidden md:block md:ml-4 md:pl-4">
-          <a href="https://github.com/tresorenville/tresorenville_website" rel="external nofollow" title="Code source sur GitHub">
-            <img src={githubMark} alt="Github Mark" className="h-6 w-6" />
-          </a>
-        </div>
       </div>
     </header>
   );
