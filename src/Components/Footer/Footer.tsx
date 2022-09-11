4f6e0323e8762ed0afb57d2665185997e5082ad9
git.youn <git.youn@gmail.com>
2022-09-11 12:42:35 +0200

    update footer contact

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return <footer>
-        <p className="text-center mt-8 py-4 text-xs dark:border-t border-gray-200 shadow-inner">2022 - Youën Ruaux</p>
+        <p className="text-center mt-8 py-4 text-xs dark:border-t border-gray-200 shadow-inner">2022 - Trésor en ville (tresorenville@pm.me)</p>
     </footer>
 }
 
